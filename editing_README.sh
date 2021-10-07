83fdb8f2b00a046eb500b077a8ac057fc2cd391f
Aodaruma <skyblue.1007.sakkyoku@gmail.com>
2021-10-07 22:25:03 +0900

    migrate script and add README.md to each directories

---
+++ editing_README.sh
@@ -0,0 +1,9 @@
+code "Partition&Flex/@Partition&Flex.anm"
+for d ($(cat cped.txt)); do
+    open "${d}/README.md"
+    find "${d}" -type f -d 1 | grep -v README | xargs code 
+    open "${d}"
+    read \?"Now: ${d}; press any key when you finish to edit: "
+    sed -i -e "s/${d}//g" cped.txt
+done
+echo "Done!"
